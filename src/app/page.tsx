594c62ed7934fe1407921a0a587710808413e794
Radek Vymětalík <rvymetalik@email.cz>
2023-10-25 18:31:14 +0200

    Added MongoDB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,48 +2,48 @@ import Button from '@/components/Button'
 import { SEARCH_AUTHOR, SEARCH_VENUE } from '@/constants/urls'
 import { VenueType } from '@/enums/VenueType'
 
-export default function HomePage() {
-  return (
-    <main
-      className='grid w-full h-full py-8'>
-      <div
-        className='place-self-center flex flex-col items-center gap-9'>
-        <h2
-          className='font-black flex flex-col items-center text-center text-[min(8vw,3rem)]'>
-          Explore and Analyse <br />
-          <span className='text-primary font-black'>dblp</span>
-        </h2>
-        <p className='text-center max-w-xl'>
-          Computer science bibliography that provides open bibliographic information on major computer science journals and proceedings.
-        </p>
+export default async function HomePage() {
+    return (
+        <main
+            className='grid w-full h-full py-8'>
+            <div
+                className='place-self-center flex flex-col items-center gap-9'>
+                <h2
+                    className='font-black flex flex-col items-center text-center text-[min(8vw,3rem)]'>
+                    Explore and Analyse <br />
+                    <span className='text-primary font-black'>dblp</span>
+                </h2>
+                <p className='text-center max-w-xl'>
+                    Computer science bibliography that provides open bibliographic information on major computer science journals and proceedings.
+                </p>
 
-        <div
-          className='mt-6 flex gap-3 flex-wrap justify-center'>
-          <Button
-            size='sm'
-            href={SEARCH_AUTHOR}>
-            Authors
-          </Button>
-          <Button
-            size='sm'
-            variant='outline'
-            href={`${SEARCH_VENUE}?type=${VenueType.Journal}`}>
-            Journals
-          </Button>
-          <Button
-            size='sm'
-            variant='outline'
-            href={`${SEARCH_VENUE}?type=${VenueType.Conference}`}>
-            Conferences
-          </Button>
-          <Button
-            size='sm'
-            variant='outline'
-            href={`${SEARCH_VENUE}?type=${VenueType.Series}`}>
-            Series
-          </Button>
-        </div>
-      </div>
-    </main>
-  )
+                <div
+                    className='mt-6 flex gap-3 flex-wrap justify-center'>
+                    <Button
+                        size='sm'
+                        href={SEARCH_AUTHOR}>
+                        Authors
+                    </Button>
+                    <Button
+                        size='sm'
+                        variant='outline'
+                        href={`${SEARCH_VENUE}?type=${VenueType.Journal}`}>
+                        Journals
+                    </Button>
+                    <Button
+                        size='sm'
+                        variant='outline'
+                        href={`${SEARCH_VENUE}?type=${VenueType.Conference}`}>
+                        Conferences
+                    </Button>
+                    <Button
+                        size='sm'
+                        variant='outline'
+                        href={`${SEARCH_VENUE}?type=${VenueType.Series}`}>
+                        Series
+                    </Button>
+                </div>
+            </div>
+        </main>
+    )
 }
